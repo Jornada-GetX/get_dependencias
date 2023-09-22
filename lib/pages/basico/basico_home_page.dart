920fb364cff2c2fdf2a580314576460bec59990e
renatorv <renatorv@gmail.com>
2023-09-22 16:01:08 -0300

    ajustes layout

--- lib/pages/basico/basico_home_page.dart
+++ lib/pages/basico/basico_home_page.dart
@@ -3,7 +3,7 @@ import 'package:get/get.dart';
 import 'package:get_dependencias/pages/basico/basico_controller.dart';
 
 class BasicoHomePage extends StatelessWidget {
-  final controller = Get.put(BasicoController()) ;
+  final controller = Get.put(BasicoController());
   BasicoHomePage({super.key});
 
   @override
@@ -42,15 +42,17 @@ class WidgetInterno extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      const Text('Widget interno'),
-      const SizedBox(
-         height: 20,
-      ),
-      ElevatedButton(
-              onPressed: () => Get.find<BasicoController>().alteraNome('Renato Alves'),
-              child: const Text('Alterar nome'),
-            ),
-    ],);
+    return Column(
+      children: [
+        const Text('Widget interno'),
+        const SizedBox(
+          height: 20,
+        ),
+        ElevatedButton(
+          onPressed: () => Get.find<BasicoController>().alteraNome('Renato Alves'),
+          child: const Text('Alterar nome'),
+        ),
+      ],
+    );
   }
 }
